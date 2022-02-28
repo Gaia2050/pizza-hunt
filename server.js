c83ad0c5c55cc65ddc1d65393da78934690b6807
Gaia Guinouard-Balconi-Weida <gaiagoessocial@gmail.com>
2022-02-27 21:04:14 -0500

    updated the UI, added creation functionality and ability to add content

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/pizza-hunt', {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
+    // useNewUrlParser: true, 
+    // useUnifiedTopology: true
+});
+
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pizza-hunt', {
-    userNewUrlParser: true, 
-    useUnifiedTopology: true
-});
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
