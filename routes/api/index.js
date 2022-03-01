43c87b1baeae923cf0f0d5974da9f64b4604baf3
Gaia Guinouard-Balconi-Weida <gaiagoessocial@gmail.com>
2022-03-01 18:17:53 -0500

    bcs

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,9 +4,8 @@ const commentRoutes = require('./comment-routes');
 // const pizzaRoutes = require('./pizza-routes');
 
 //add prefix of /pizzas to routes created in pizza-routes.js
-router.use('pizzas', pizzaRoutes);
+router.use('/pizzas', pizzaRoutes);
 router.use('/comments', commentRoutes);
-router.use('pizzas', pizzaRoutes);
 
 
 module.exports = router;
